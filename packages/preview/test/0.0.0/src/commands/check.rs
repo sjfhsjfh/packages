ce79d4b394792b69a7e23135b163526e1091eddd
sjfhsjfh <872286346@qq.com>
2025-01-10 01:43:03 +0800

    [Typship] Add src/commands/check.rs

---
+++ packages/preview/test/0.0.0/src/commands/check.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+
+use anyhow::Result;
+use clap::Command;
+use log::info;
+
+use crate::utils::read_manifest;
+
+pub fn cmd() -> Command {
+    Command::new("check")
+        .about("Check if the package is valid (WIP).")
+        .long_about("Check if the package is valid (WIP). Must be in the package directory.")
+}
+
+pub fn check(package_dir: &Path) -> Result<()> {
+    let _current = read_manifest(package_dir)?;
+    // TODO: warn empty fields? check glob?
+    // TODO: move init validations here (or use a common function)
+    info!("No issues found");
+    Ok(())
+}
